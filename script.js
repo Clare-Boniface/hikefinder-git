1687e1bb085fa7e3a31070b0c5f0f342eb122325
Clare-Boniface <clarelgarfield@hotmail.com>
2023-08-05 14:08:19 +0100

    remove redundant image files

--- script.js
+++ script.js
@@ -3,11 +3,9 @@ const mobileMenu = document.getElementById("mobile-nav");
 const closeBtn = document.getElementById("close-btn");
 
 hamburgerMenuIcon.addEventListener("click", function () {
-  console.log("hamburger clicked");
   mobileMenu.classList.remove("hidden");
 });
 
 closeBtn.addEventListener("click", function () {
-  console.log("close button clicked");
   mobileMenu.classList.add("hidden");
 });
